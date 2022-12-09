7ac2ab73fd8d4626aadf6959abaaba217424b7c5
Ghost_chu <30802565+Ghost-chu@users.noreply.github.com>
2022-12-10 01:01:18 +0800

    Use Ghost-chu/qBittorrent as qBittorrent sources

--- qbittorrent-nox-static.sh
+++ qbittorrent-nox-static.sh
@@ -613,8 +613,8 @@ set_module_urls() {
 	libtorrent_github_tag_default="$(grep -Eom1 "v${qbt_libtorrent_version}.([0-9]{1,2})" <<< "${libtorrent_github_tags_list}")"
 	libtorrent_github_tag="${libtorrent_github_tag:-$libtorrent_github_tag_default}"
 
-	qbittorrent_github_url="https://github.com/qbittorrent/qBittorrent.git"
-	qbittorrent_github_tag_default="$(git_git ls-remote -q -t --refs https://github.com/qbittorrent/qBittorrent.git | awk '{sub("refs/tags/", "");sub("(.*)(-[^0-9].*|rc|alpha|beta)(.*)", ""); print $2 }' | awk '!/^$/' | sort -rV | head -n1)"
+	qbittorrent_github_url="https://github.com/Ghost-chu/qBittorrent.git"
+	qbittorrent_github_tag_default="$(git_git ls-remote -q -t --refs https://github.com/Ghost-chu/qBittorrent.git | awk '{sub("refs/tags/", "");sub("(.*)(-[^0-9].*|rc|alpha|beta)(.*)", ""); print $2 }' | awk '!/^$/' | sort -rV | head -n1)"
 	qbittorrent_github_tag="${qbitorrent_github_tag:-$qbittorrent_github_tag_default}"
 
 	#### Gihub Workflow URLS ##########################################################################################################################
